b2a35f03f1097d4407b5a7f03afc97b06043237d
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2023-10-27 21:35:08 +0800

    10/27

---
+++ functions.py
@@ -0,0 +1,39 @@
+from cgi import test
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import numpy as np
+import matplotlib as plt
+import os
+import random
+import copy
+import MyModel
+import shutil
+from PIL import Image
+from collections import Counter
+
+
+def getStat(train_data):
+    '''
+    Compute mean and variance for training data
+    :param train_data: 自定义类Dataset(或ImageFolder即可)
+    :return: (mean, std)
+    '''
+    print('Compute mean and variance for training data.')
+    print(len(train_data))
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=1, shuffle=False, num_workers=0,
+        pin_memory=True)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    for X, _ in train_loader:
+        for d in range(3):
+            mean[d] += X[:, d, :, :].mean()
+            std[d] += X[:, d, :, :].std()
+    mean.div_(len(train_data))
+    std.div_(len(train_data))
+
+    return list(mean.numpy()), list(std.numpy())
